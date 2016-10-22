8939135f14e2304f6104e87c0525125282ce42e8
Henrique <henriique.ti@gmail.com>
2016-10-22 09:01:03 -0200

    correção auth Aluno

--- app/Http/Controllers/Alunos/AuthController.php
+++ app/Http/Controllers/Alunos/AuthController.php
@@ -9,5 +9,15 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+   	use AuthenticatesUsers;
+
+    protected $redirectTo = 'aluno';
+    protected $guard = 'web_students';
+    protected $loginArea = 'aluno/login';
+
+   
+    public function index(): View
+    {
+    	return view('aluno.index');
+    }
 }
