8079679644c0ac5af51a90298bbadba865e37b94
Henrique <henriique.ti@gmail.com>
2016-11-02 17:54:29 -0200

    ordem desc e navbar em relatorio

--- resources/views/admin/relatorios.blade.php
+++ resources/views/admin/relatorios.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	<title>Relatórios</title>
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+	<a class="navbar-brand" href="/home">Inicio</a>
+	</nav>
 <div class="container" style="text-align: center;">
 	<h1>Relatórios</h1>
 
